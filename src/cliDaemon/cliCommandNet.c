a0f91da0cbd030c257919d02cb5a681148cccf17
mohammad mahdi sattari <mamadsadtari83@gmail.com>
2024-02-22 13:33:11 -0500

    add tx and rx traffic command handler

---
+++ src/cliDaemon/cliCommandNet.c
@@ -0,0 +1,26 @@
+#include <libcli.h>
+
+#include "cliDaemon/cliCommandNet.h"
+#include "cliDaemon/cliIpcHandler.h"
+#include "common/commonIpcHandler.h"
+#include "common/logger.h"
+
+int
+_get_tx_traffic_command(struct cli_def *cli,const char *command, char *argv[], int argc)
+{
+    char res[MAX_RES_LEN] = {0};
+    if(handle_cli_ipc(TX_TRAFFIC_REQ, res, MAX_RES_LEN) < 0)
+        {__LOG_WARN__("Cli Ipc Handler Failed\n"); return CLI_ERROR;}
+    cli_print(cli, "%s", res);
+    return CLI_OK;
+}
+
+int
+_get_rx_traffic_command(struct cli_def *cli,const char *command, char *argv[], int argc)
+{
+    char res[MAX_RES_LEN];
+    if(handle_cli_ipc(RX_TRAFFIC_REQ, res, MAX_RES_LEN) < 0) 
+        {__LOG_WARN__("Cli Ipc Handler Failed\n"); return CLI_ERROR;}
+    cli_print(cli, "%s", res);
+    return CLI_OK;
+}
