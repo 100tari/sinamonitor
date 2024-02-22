affe1b4dedfa69fde25cdd09c349da35c2e1316d
mohammad mahdi sattari <mamadsadtari83@gmail.com>
2024-02-22 12:25:28 -0500

    implement Gather Data Daemon using gather data net and gather data ipc
    it is necessary ipc handler works properly and if it failed daemon will
    faile but if gather data net failes daemon can keep running

--- include/gatherDataDaemon/gatherDataDaemon.h
+++ include/gatherDataDaemon/gatherDataDaemon.h
@@ -1,4 +1,7 @@
 #ifndef _GATHER_DATA_DAEMON_H_
 #define _GATHER_DATA_DAEMON_H_
 
+int             init_gather_data_daemon();
+void            run_gather_data_daemon();
+
 #endif // _GATHER_DATA_DAEMON_H_
