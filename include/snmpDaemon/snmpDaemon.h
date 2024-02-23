f21f423e42b859a444bc6de693c4b411f2bd4249
mohammad mahdi sattari <mamadsadtari83@gmail.com>
2024-02-23 05:49:18 -0500

    add snmpDaemon init and run functions using ipc handler and NET module

--- include/snmpDaemon/snmpDaemon.h
+++ include/snmpDaemon/snmpDaemon.h
@@ -1,4 +1,13 @@
 #ifndef _SNMP_DAEMON_H_
 #define _SNMP_DAEMON_H_
 
+/* note: the name of conf file must be exactly same as this macro */
+#define SNMP_MASTER_AGENT_NAME              "master-agent"
+
+/* if this macro set to nonzero value, the program will be pure daemon */
+#define SNMP_MASTER_AGENT_BECOME_DAEMON     0
+
+int                     init_snmp_daemon(const char* agentName);
+void                    run_snmp_daemon(const char* agentName);
+
 #endif // _SNMP_DAEMON_H_
